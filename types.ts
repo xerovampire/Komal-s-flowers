8f96a2fa2eb9c45511571d102002a9d9540ba49a
xerovampire <160816502+xerovampire@users.noreply.github.com>
2026-02-06 21:36:50 +0530

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,11 @@
+export enum Screen {
+  PROPOSAL = 'PROPOSAL',
+  GIFT_FLOWER = 'GIFT_FLOWER',
+}
+
+export interface FloatingHeart {
+  id: number;
+  left: number;
+  animationDuration: number;
+  delay: number;
+}
